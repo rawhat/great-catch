a819b868b5add5244624623481d169ef806cd7cd
alex manning <awm48@drexel.edu>
2017-02-07 01:30:42 -0500

    updated server api

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+var path = require('path');
+var _ = require('lodash');
 var koa = require('koa');
 var app = new koa();
 
 var router = require('koa-router')();
 
 var convert = require('koa-convert');
-app.use(convert(require('koa-static')(__dirname + '/static')));
+app.use(convert(require('koa-static')(path.join(__dirname, 'static'))));
 app.use(convert(require('koa-better-body')()));
 
 var views = require('koa-views');
 
-app.use(views(__dirname + '/views', {
+app.use(views(path.join(__dirname, 'views'), {
   map: {
     pug: 'pug'
   }
@@ -23,6 +25,9 @@ var SESSION_KEYS = ['thisisatestkeyreplacewithbetterlater'];
 // app.use(passport.initialize());
 // app.use(passport.session());
 
+
+// uniqueness constraint for property
+// CREATE CONSTRAINT ON (u:User) ASSERT u.username IS UNIQUE;
 var neo4j = require('neo4j-driver').v1;
 
 const makeDBQuery = async ({ queryString, object }) => {
@@ -32,12 +37,13 @@ const makeDBQuery = async ({ queryString, object }) => {
         let response = await session.run(queryString, object);
         session.close();
         driver.close();
-        return response.records[0];
+        return { records: response.records };
     }
-    catch (e) {
-        console.error(e);
+    catch (error) {
+        console.error(error);
+        return { error };
     }
-}
+};
 
 let users = [
     { name: 'alex', password: 'test', id: 1 },
@@ -60,47 +66,98 @@ const jwt = koaJwt({ secret: SESSION_KEYS[0] });
 const jwebtoken = require('jsonwebtoken');
 // app.use(convert(jwt({ secret: SESSION_KEYS[0], passthrough: true }).unless({ path: ['/', '/login']})));
 
-router.get('/users/:username', async (ctx, next) => {
-    let queryResults = await makeDBQuery({
-        queryString: 'MATCH (a:User) WHERE a.username = {username} RETURN a',
-        object: { username: ctx.params.username }
-    });
-
-    ctx.body = 'Got back: ' + queryResults;
-});
-
 router.post('/login', async (ctx, next) => {
     console.log(ctx.request.fields);
 
+    let { username, password } = ctx.request.fields;
+
+    // fields:
+    //  { username, password }
+
     // DO AUTHENTICATION HERE, THEN PULL USER INFO FROM DB
-    //   -- CONSIDER REPLACING WITH PASSPORT LOCAL STRATEGY
-    let user = users.find(u => u.name === ctx.request.fields.username);
-                                                    // seconds
-    let token = jwebtoken.sign(user, SESSION_KEYS[0], { expiresIn: 60 * 60 * 5 });
-    ctx.body = { token, username: user.username };
-    ctx.status = 200;
+
+    let { records, error} = await makeDBQuery({
+        queryString: 'MATCH (u:User) WHERE u.username = {username} AND u.password = {password} RETURN u AS user',
+        object: {
+            username,
+            password
+        }
+    });
+
+    // check if no errors AND match in DB
+    if(!error && records.length) {
+        let user = records[0].get('user').properties;
+
+        //   -- CONSIDER REPLACING WITH PASSPORT LOCAL STRATEGY
+        // let user = users.find(u => u.name === ctx.request.fields.username);
+        //                                                 // seconds
+        let token = jwebtoken.sign(user, SESSION_KEYS[0], { expiresIn: 60 * 60 * 5 });
+        ctx.body = { token, username: user.username };
+        ctx.status = 200;
+    }
+    // extend this to check for specific errors
+    else {
+        ctx.status = 401;
+    }
 });
 
-router.get('/user/:id/profile', jwt, async (ctx, next) => {
-    console.log(ctx);
-    console.log(ctx.params.id);
+router.get('/user/:id/profile', async (ctx, next) => {
     // GET USER DATA FROM DB HERE
-    let userData = {
-        username: 'test',
-        dateOfBirth: '01/01/1980',
-        fullName: 'Test User'
-    };
+    let { id } = ctx.params;
 
-    ctx.body = userData;
+    let { records, error } = await makeDBQuery({
+        queryString: 'MATCH (u:User) WHERE id(u) = {id} RETURN u AS user',
+        object: {
+            id: parseInt(id)
+        }
+    });
+
+    // let userData = {
+    //     username: 'test',
+    //     dateOfBirth: '01/01/1980',
+    //     fullName: 'Test User'
+    // };
+
+    if(!error && records.length) {
+        let userData = _.omit(records[0].get('user').properties, 'password');
+        ctx.body = userData;
+    }
+    else {
+        ctx.status = 403;
+    }
 });
 
 router.post('/user/create', async (ctx, next) => {
-    // CREATE USER IN DB, THEN USE IT TO SIGN JWT INSTEAD
-    let user = Object.assign({}, ctx.request.fields, { id: users.length + 1 });
-    users = users.concat(user);
-    let token = jwebtoken.sign(user, SESSION_KEYS[0], { expiresIn: 60 * 60 * 5 });
+    let { username, password, email } = ctx.request.fields;
 
-    ctx.body = { token };
+    // fields:
+    //  { username, password, password_repeat, email }  -- any more?
+
+    // CREATE USER IN DB, THEN USE IT TO SIGN JWT INSTEAD
+
+    let { records, error } = await makeDBQuery({ 
+        queryString: 'CREATE (u:User { username: {username}, password: {password}, email: {email}}) RETURN u AS user', 
+        object: {
+            username,
+            password,
+            email
+        }
+    });
+
+    if(!error) {
+        let user = _.omit(records[0].get('user').properties, password);
+        // let user = Object.assign({}, ctx.request.fields, { id: users.length + 1 });
+        users = users.concat(user);
+
+        // let token = jwebtoken.sign(user, SESSION_KEYS[0], { expiresIn: 60 * 60 * 5 });
+
+        // ctx.body = { token };
+        console.log('user', user);
+        ctx.body = 'user created';
+    }
+    else {
+        console.error(error);
+    }
 });
 
 router.get('*', async (ctx, next) => {
