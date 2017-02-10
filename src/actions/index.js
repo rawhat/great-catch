8eea9ee4554f2643a29314cd0be5e23446ee41c9
Alex Manning <alex41290@gmail.com>
2017-02-10 16:47:19 -0500

    fixed stupid url typo

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ import {
     FETCH_USER_DATA
 } from './types';
 
-let SERVER_URL = 'http://greatcatchhelp.comg';
+let SERVER_URL = 'http://greatcatchhelp.com';
 if(process.env.NODE_ENV == 'production') SERVER_URL = 'http://greatcatchhelp.com';
 
 export function loginUser(formData) {
