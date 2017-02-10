d9edafd1507a28d02da06f9c7b59556cc0f732e2
Alex Manning <alex41290@gmail.com>
2017-02-10 12:52:25 -0500

    testing manual oauth

--- routes.js
+++ routes.js
@@ -188,8 +188,18 @@ router.get('/auth/fitbit/callback', async (ctx) => {
             let response = await request.request();
             console.log(response.data);
         }
-        catch (e) {
-            console.error(e.request.data.errors);
+        catch (error) {
+            if (error.response) {
+                // The request was made, but the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
         }
     }
 });
