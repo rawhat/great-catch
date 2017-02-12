91d0ba3af18d2f4af272a9da2b1affe14f713d7e
williamxin <wx28@drexel.edu>
2017-02-12 04:53:52 +0000

    stat.js created online with Bitbucket

---
+++ src/components/stat.js
@@ -0,0 +1,100 @@
+/*
+	this is statical analysis module
+
+	using this simple-statistics package
+	API doc: 
+	http://simplestatistics.org/docs/
+	
+	last mod 02/11/17
+	
+	program logic:
+	conduct linear regression for new and old data and compare the result 
+	2 cases
+	1) if new data slope is higher than old data slope
+		- display "good job, keep it up!"
+	2) if new data slope if lower or equal to old data slope
+		- calculate mean absolute deviation (MAD) of old data and compare to new data
+			- if new data lower than MAD, display "lazy bum need more exercise!"
+			- else display "not bad, you can do better!"
+
+	NOTE: ONLY prototype, need more complicated statistical analysis algorithm
+*/
+
+// require stat package
+var stat = require("simple-statistics")
+
+module.exports = {
+	/*
+		calculate mean absolute deviation
+		input:
+			yData: y axis data
+		output:
+			MAD: mean absolute deviation
+	*/
+	calcMAD: function calcMAD(yData){
+		// get mean
+		var mean = stat.mean(yData);
+		
+		// set variables
+		var numerator = 0;
+		var denominator = yData.length;
+		
+		// calc absolute difference from mean
+		for (var i = 0; i < denominator; i++){
+			numerator += Math.abs(yData[i] - mean);
+		}
+		// return MAD
+		return numerator/denominator;
+	},
+
+	/*
+		calculate linear regression
+		input: 
+			yData: y axis data
+		output: 
+			regrObj.m: the slow attribute of the linearRegression return object
+	*/
+	calcLinearRegr: function calcLinearRegr(yData){
+		// create x axis data
+		xData = [];
+		for (var i = 0; i < yData.length; i++){
+			xData.push(i);
+		}
+		// calc linear regression
+		var regrObj = stat.linearRegression([xData, yData]);
+		
+		// return slope
+		return regrObj.m;
+	},
+
+	/*
+		determine case
+		input: 
+			newData: today's data
+			oldData: past data
+			TODO: oldData length to be determined
+		output:
+			console log messages
+			NOTE: change to whatever as needed
+	*/
+	deterResult: function deterResult(oldData, newData){
+		// get slope of old data
+		var oldSlope = module.exports.calcLinearRegr(oldData);
+		
+		// get slope of old data + new data
+		var newSlope = module.exports.calcLinearRegr(oldData.concat(newData));
+
+		// if new slope is equal or lower than old slope
+		if (newSlope <= oldSlope){
+			// compare MAD
+			if (newData <= module.exports.calcMAD(oldData)){
+				console.log("lazy bum, go exercise!");
+				return;
+			}
+		}
+	
+		// anything else
+		console.log("good job, keep it up!");
+		return;
+	}
+};
