42981490ad93febe86657151b7367d5503e48b2c
Wenyu <wenyuxin28@126.com>
2017-03-09 16:01:46 -0500

    dynamically load information on profile.js

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,5 +1,12 @@
 // functions
 $(document).ready(function(){
-	var email = sessionStorage.getItem('email')
-	console.log(email);
+	var email = sessionStorage.getItem('email');
+	var username = sessionStorage.getItem('userName');
+	
+	// TODO: this is supid, need to change the DB to store first/last name
+	var names = username.split(/(?=[A-Z])/);
+	$("#userName").empty().append(names[0] + " " + names[1] + "!");
+	$("#firstName").empty().append(names[0]);
+	$("#lastName").empty().append(names[1]);
+	$("#eAddress").empty().append(email);
 })
