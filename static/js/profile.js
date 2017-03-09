4892abbf0293a72a45a66db7f98ea85812e26ffd
Wenyu <wenyuxin28@126.com>
2017-03-09 16:26:11 -0500

    attempt to do api/fitbit request, half way done, to be finished with .post

--- static/js/profile.js
+++ static/js/profile.js
@@ -9,4 +9,31 @@ $(document).ready(function(){
 	$("#firstName").empty().append(names[0]);
 	$("#lastName").empty().append(names[1]);
 	$("#eAddress").empty().append(email);
+	
+	$("#request_fitbit_data").click(function(){
+		var dataType = $('[name="fitbit_dataType"] option:selected').val()
+		var range = $('[name="range"] option:selected').val()
+		var startDate = $('[name="selected_date"]').val()
+
+		$.post(
+			// url
+			"/api/fitbit",
+			// data
+			{ 
+				data_set: dataType,
+				date: startDate,
+				period: range	
+			},
+			// function after data received
+			function(data, status){
+				// TODO: exact function to be implemented
+				if (status == "success"){
+					console.log("do something");
+				}else{
+					alert("Failed");
+				}
+			}
+			
+		)
+	});
 })
