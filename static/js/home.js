9ad874154bcbd5bb8a2af1eae8ceb499d7604e80
Wenyu <wenyuxin28@126.com>
2017-03-09 13:10:50 -0500

    added session storage for session token and alert for create user function

--- static/js/home.js
+++ static/js/home.js
@@ -2,26 +2,36 @@
 $(document).ready(function(){
 	// when you click on submit
 	$("#signUn_submit").click(function(){
+		// parse all information
 		var firstName = $('[name="firstname"]').val();
 		var lastName = $('[name="lastname"]').val();
 		var email = $('[name="email"]').val();
 		var passWord = $('[name="pw"]').val();
 		
+		// send a post to /user/create endpoint
 		$.post(
-			"http://localhost:3000/user/create",
+			// url
+			"/user/create",
+			// data
 			{ 
 				username: firstName+lastName,
 				email,
 				password: passWord,
 				password_repeat: passWord	
 			},
-			function(response){
-				if (response.errors){
-					console.log("error");
+			// function after data received
+			function(data, status){
+				// TODO: exact function to be implemented
+				if (status == "success"){
+					alert("Success");
+					// token manage
+					sessionStorage.setItem("sessionToken", data.token);
+					console.log(sessionStorage.getItem("sessionToken"));
 				}else{
-					console.log(response.data);
+					alert("Failed");
 				}
 			}
+			
 		)
 		
 	});
