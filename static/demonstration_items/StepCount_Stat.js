368b6d941e20dd2413fc5bbf0ba550ec800da579
Wenyu <wenyuxin28@126.com>
2017-04-18 15:23:15 -0400

    added callbacks to link 2 http calls, still have issue return final results

--- static/demonstration_items/StepCount_Stat.js
+++ static/demonstration_items/StepCount_Stat.js
@@ -124,8 +124,8 @@ function deterResult(originData, oldData){
 	// if new slope is equal or lower than old slope
 	if (newSlope < oldSlope){
 		// TODO: this is here for testing, will need to pass parameter over
-		weatherCheck(19104);
-		drugCheck("LETAIRIS");
+		var result = weatherCheck(19104, "LETAIRIS", drugCheck);
+		return result;
 	}else{
 		return ("normal w/ linear regression");
 	}
@@ -199,7 +199,7 @@ function parseData(data){
 	TODO:
 		figure out how to link with main analysis
 */
-function weatherCheck(zipcode){
+function weatherCheck(zipcode, drug, callback){
 	var key = "971c72f24410bd75";
 	var date = new Date();
 	var date = date.getFullYear().toString() + ("0" + (date.getMonth() + 1).toString()).slice(-2) + ("0" + date.getDate().toString()).slice(-2)
@@ -214,7 +214,7 @@ function weatherCheck(zipcode){
 			parsed = JSON.parse(body);
 			var mainInfo = parsed.history.dailysummary[0];
 			var weatherSum = parseInt(mainInfo.fog) + parseInt(mainInfo.rain) + parseInt(mainInfo.snow) + parseInt(mainInfo.hail) + parseInt(mainInfo.thunder) + parseInt(mainInfo.tornado);
-			console.log(weatherSum);
+			callback(drug, weatherSum);
 		});
 	});
 }
@@ -225,12 +225,10 @@ function weatherCheck(zipcode){
 		drug: medicine user take
 	output:
 		nComplain: number of complains for fatigue for this medicine
-	
-	TODO: fix this
 */
-function drugCheck(drug){
+function drugCheck(drug, weather){
 	var key = "kZ1dIlu9TyKAzlXidiBuejvdfXmQmLWpq2BF0wqY";
-	url = "https://api.fda.gov/drug/event.json?api_key=" + key + "&search=reaction.reactionmeddrapt.exact='Fatigue'+AND+brand_name:'LETAIRIS'" + drug;
+	url = "https://api.fda.gov/drug/event.json?api_key=" + key + "&search=reaction.reactionmeddrapt.exact=" + "Fatigue" + "+AND+brand_name:" + drug;
 	https.get(url, function(res){
 		var body = "";
 		var parsed;
@@ -238,11 +236,35 @@ function drugCheck(drug){
 			body += data;
 		});
 		res.on('end', function(){
-			parsed = JSON.parse(body);
-			console.log(parsed);
+			var parsed = JSON.parse(body);
+			var count = parsed.meta.results.total;
+			correlationAlert(count, weather);
 		});
-	});
-	
+	});	
+}
+
+/*
+	correlation alert summary
+	input:
+		drug: FDA drug api count for fatigue complain for a particular drug
+		weather: weather factor sum
+	output:
+		msg: indicate any of the finding
+*/
+function correlationAlert(drug, weather){
+	var nothing = "We have found no reason why your step count decreased, stop being lazy"
+	var starter = "We have found ";
+	var drugMsg = "your medicine is making you fatigue";
+	var weatherMsg = "today's weather around your location is bad";
+	if (drug > 0 && weather > 0){
+		console.log( starter + drugMsg + "AND " + weatherMsg);
+	}else if (drug > 0 && weather == 0){
+		console.log( starter + drugMsg);
+	}else if (drug == 0 && weather == 0){
+		console.log( nothing);
+	}else if (drug == 0 && weather > 0){
+		console.log( starter + weatherMsg);
+	}
 }
 
 module.exports = {
