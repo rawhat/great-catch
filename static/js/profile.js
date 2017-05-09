4dd8855fe8ce989941441de08845947e431f0b20
Wenyu <wenyuxin28@126.com>
2017-05-09 15:53:47 -0400

    integrated demo to profile page, still need to sort out a few thing on the front end, also need to swap out linear regression with STD

--- static/js/profile.js
+++ static/js/profile.js
@@ -15,12 +15,120 @@ $(document).ready(function(){
 	var email = sessionStorage.getItem('email');
 	var username = sessionStorage.getItem('userName');
 
-	// TODO: this is supid, need to change the DB to store first/last name
-	var names = username.split(/(?=[A-Z])/);
-	$("#userName").empty().append(names[0] + " " + names[1] + "!");
-	$("#firstName").empty().append(names[0]);
-	$("#lastName").empty().append(names[1]);
-	$("#eAddress").empty().append(email);
+	// TODO: uncomment this once I am done with profile analysis button
+	//var names = username.split(/(?=[A-Z])/);
+	//$("#userName").empty().append(names[0] + " " + names[1] + "!");
+	//$("#firstName").empty().append(names[0]);
+	//$("#lastName").empty().append(names[1]);
+	//$("#eAddress").empty().append(email);
+	
+	// click on show profile analysis button
+	$('#profileAnalysis').click(function(){
+		// TODO: replace this data with a get from server, return a data variable called data in json format
+		data = {
+				"steps":[1139, 1400, 1211, 1310, 1499, 1257, 1510, 1303, 1215, 1244, 1550, 1220, 1120, 1220, 900],
+				"steps_label": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				"heartRates":[69, 68, 68, 72, 70, 69, 68, 69, 71, 72, 69, 68, 68, 69, 71],
+				"heartRates_label":[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				"date":["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", "Day 14"],
+				"drug":"N/A",
+				"zip":19104
+		}; 
+		$('#assessment').text('');
+		$('#assessment').removeClass('alert alert-info alert-warning');
+		var chart = $("#myChart");
+		var myChart = new Chart(chart, {
+			type: 'line',
+			data: {
+				labels: data.date,
+				datasets: [{
+					label: 'Step Count',
+					data: data.steps,
+					backgroundColor: 'rgba(255, 99, 132, 0.2)',
+					fill: false,
+					pointDotRadius: 10
+				},{
+					label: 'Step Count + 1 STD',
+					data: [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500],
+					backgroundColor: 'rgba(54, 162, 235, 1)',
+					fill: false,
+					pointDotRadius: 10
+				},{
+					label: 'Step Count - 1 STD',
+					data: [900, 900, 900, 900, 900, 900, 900, 900, 900, 900, 900, 900, 900, 900, 900],
+					backgroundColor: 'rgba(255,99,132,1)',
+					fill: false,
+					pointDotRadius: 10
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						},
+						scaleLabel: {
+							display: true,
+							labelString: 'Step Count (n) / Heart Rate (bpm)'
+						}
+					}]
+				}
+			}
+		});
+		
+		var chart2 = $("#myChart2");
+		var myChart = new Chart(chart2, {
+			type: 'line',
+			data: {
+				labels: data.date,
+				datasets: [{
+					label: 'Heart Rate',
+					data: data.heartRates,
+					backgroundColor: 'rgba(75, 192, 192, 0.2)',
+					fill: false,
+					pointDotRadius: 10
+				},{
+					label: 'Heart Rate + 1 STD',
+					data: [60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60],
+					backgroundColor: 'rgba(255, 206, 86, 1)',
+					fill: false,
+					pointDotRadius: 10
+				},{
+					label: 'Heart Rate - 1 STD',
+					data: [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50],
+					backgroundColor: 'rgba(75, 192, 192, 1)',
+					fill: false,
+					pointDotRadius: 10
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						},
+						scaleLabel: {
+							display: true,
+							labelString: 'Step Count (n) / Heart Rate (bpm)'
+						}
+					}]
+				}
+			}
+		});
+
+		$.post('/analysis/step', {
+			data
+		}).then((res) => {
+			$('#assessment').html(res);
+			if(res.search('AND') === -1) {
+				$('#assessment').addClass('alert alert-info');
+			}
+			else {
+				$('#assessment').addClass('alert alert-warning');
+			}
+		});
+		
+	});
 
 	$("#request_fitbit_data").click(function(){
 		var dataType = $('[name="fitbit_dataType"] option:selected').val()
