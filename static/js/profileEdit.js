547f94375e37edbf0f725b1a7572c0d6c88af484
Wenyu Xin <wx28@cs.drexel.edu>
2017-05-18 14:34:41 -0400

    made some front end change to editprofile page

--- static/js/profileEdit.js
+++ static/js/profileEdit.js
@@ -1,6 +1,45 @@
-if(localStorage.tasks!="")
-  document.getElementById("EmailList").innerHTML=localStorage.tasks;
+$(document).ready(function(){
+	// TODO: do a ajax request to get this json file
+	data = {
+	"_COMMENTS": "ABNORMAL step w/ weather and drug, ABNORMAL heart rate w/ step and drug, prednisone is a steroid that treat many diseases, focus on inflammation",
+	"firstName": "Daniel",
+	"lastName": "Grayson",
+	"email": "Daniel.Grayson@drexel.edu",
+	"emergencyContact": "TinnaWang@aol.com",
+	"occupation": "90 Years Old Man",
+	"steps":[1139, 1400, 1211, 1310, 1499, 1257, 1510, 1303, 1215, 1244, 1550, 1220, 1120, 1220, 1000],
+	"stepSTD": 137.99,
+	"heartRates":[72, 68, 68, 72, 70, 69, 68, 69, 71, 72, 69, 68, 68, 69, 73],
+	"heartRatesSTD": 1.61,
+	"date":["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", "Day 14", "Day 15"],
+	"drug":"prednisone",
+	"zip":50002	
+	}; 
+	
+	$("#firstName").empty().append(data.firstName);
+	$("#lastName").empty().append(data.lastName);
+	$("#eAddress").empty().append(data.email);
+	$("#eContacts").empty().append(data.emergencyContact);
+	$("#Medicine").empty().append(data.drug);
+	$("#zipCode").empty().append(data.zip);
+	$("#occp").empty().append(data.occupation);
+	
+	$('#viewProfile').click(function(){
+		window.location.href = "profile.html";
+	})
+	
+	$('#savebtn').click(function(){
+		//TODO: extract info and send to server
+	})
+	
+});
 
+
+
+//if(localStorage.tasks!="")
+//  document.getElementById("EmailList").innerHTML=localStorage.tasks;
+
+/*
 function addEmail(){
   var taskcontent=document.getElementById("emailContent").value;
   var taskbox=document.createElement("div");
@@ -13,15 +52,11 @@ function addEmail(){
   // var inputcheckbox=document.createElement("input");
   // inputcheckbox.type="checkbox";
   // taskbox.appendChild(inputcheckbox);
+*/
 
+  
 
-  if (window.localStorage) {
-      localStorage.setItem("tasks",document.getElementById("list").innerHTML);
-    } else {
-      Cookie.write("tasks", document.getElementById("list").innerHTML);
-    }
-}
-
+/*
 function addMedicine(){
   var taskcontent=document.getElementById("medicineContent").value;
   var taskbox=document.createElement("div");
@@ -30,6 +65,5 @@ function addMedicine(){
   var inputtext=document.createElement("label");
   inputtext.innerHTML=taskcontent;
   taskbox.appendChild(inputtext);
-
-
 }
+*/
